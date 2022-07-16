86a4d89292d40568897b54fea00d039606dacc4e
SparrowHawkEye <makh0043@gmail.com>
2022-07-17 01:25:53 +0600

    signin page done

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,44 +1,52 @@
 import { useState } from "react";
 import {
-  createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
+  signInWithGooglePopup,
+  signInAuthUserWithEmailAndPassword,
 } from "../../utils/firebase/firebase.utils";
+
 import Button from "../button/button.component";
 
 import FormInput from "../form-input/form-input.component";
 
-import './sign-up-form.styles.scss'
+import "./sign-in-form.styles.scss";
 const defaultFormField = {
-  displayName: "",
   email: "",
   password: "",
-  confirmPassword: "",
 };
-const SignUpForm = () => {
+const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormField);
-  const { displayName, email, password, confirmPassword } = formFields;
+  const { email, password } = formFields;
 
   const resetFormField = () => {
     setFormFields(defaultFormField);
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    if (password !== confirmPassword) {
-      alert("passwords do not match");
-      return;
-    }
-
     try {
-      const { user } = await createAuthUserWithEmailAndPassword(
+      const response = await signInAuthUserWithEmailAndPassword(
         email,
         password
       );
-      await createUserDocumentFromAuth(user, { displayName });
+      console.log(response);
       resetFormField();
     } catch (error) {
-      console.error("User Creation Encountered an Error", error);
+      switch (error.code) {
+        case "auth/wrong-password":
+          alert("Incorrect Password for Email");
+          break;
+        case "auth/user-not-found":
+          alert("No User Associated with this Email");
+          break;
+        default:
+          console.log(error);
+      }
     }
+  };
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
   };
 
   const handleChange = (e) => {
@@ -47,18 +55,10 @@ const SignUpForm = () => {
   };
 
   return (
-    <div className="sign-up-container">
-    <h2>Don't Have an account?</h2>
-      <span>Sign Up with your email and password</span>
+    <div className="sign-in-container">
+      <h2>I already Have an account?</h2>
+      <span>Sign Ip with your email and password</span>
       <form onSubmit={handleSubmit}>
-        <FormInput
-          label="Display Name"
-          type="text"
-          required
-          onChange={handleChange}
-          name="displayName"
-          value={displayName}
-        />
         <FormInput
           label="Email"
           type="email"
@@ -75,19 +75,16 @@ const SignUpForm = () => {
           name="password"
           value={password}
         />
-        <FormInput
-          label="Confirm Password"
-          type="password"
-          required
-          onChange={handleChange}
-          name="confirmPassword"
-          value={confirmPassword}
-        />
 
-        <Button type="submit">Sign Up</Button>
+        <div className="buttons-container">
+          <Button type="submit">Sign In</Button>
+          <Button type='button' onClick={signInWithGoogle} buttonType="google">
+            Google Sign In
+          </Button>
+        </div>
       </form>
     </div>
   );
 };
 
-export default SignUpForm;
+export default SignInForm;
